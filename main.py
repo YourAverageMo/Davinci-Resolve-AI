2f5e301537821a2bc2d8e095c573d22d01d9af12
Muhammed Yilmaz <yilmaz@email.arizona.edu>
2023-11-26 01:36:04 -0700

    linting main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def parse_json(input_file):
     return parsed_data
 
 
-def seconds_to_timecode(seconds, frame_rate=60):
+def to_timecode(seconds, frame_rate=60):
     # Calculate the total number of frames
     total_frames = int(seconds * frame_rate)
 
@@ -26,7 +26,7 @@ def seconds_to_timecode(seconds, frame_rate=60):
     hours = total_frames // (frame_rate * 60 * 60)
     minutes = (total_frames // (frame_rate * 60)) % 60
     seconds = (total_frames // frame_rate) % 60
-    frames = total_frames % frame_rate  # this is always going to be 0
+    frames = total_frames % frame_rate  # this is always going to be 0 since input is seconds
 
     # Format the timecode as HH:MM:SS:FF
     timecode = f"{hours:02d}:{minutes:02d}:{seconds:02d}:{frames:02d}"
@@ -34,7 +34,7 @@ def seconds_to_timecode(seconds, frame_rate=60):
     return timecode
 
 
-def map_event_type_to_color(event_type):
+def color_mapping(event_type):
     return {
         "kill": "Blue",
         "assist": "Green",
@@ -42,31 +42,30 @@ def map_event_type_to_color(event_type):
     }.get(event_type.lower())
 
 
-def generate_timeline_markers(parsed_data):
+def gen_markers(parsed_data):
     timeline_markers = []
     for index, event in enumerate(parsed_data, start=1):
         # Use the template structure from the "output" EDL file format
-        marker_color = map_event_type_to_color(event['type'])
-        marker_start_time_seconds = event['trigger_time']
-        marker_start_time_tc = seconds_to_timecode(marker_start_time_seconds)
+        marker_color = color_mapping(event['type'])
+        trigger_time = event['trigger_time']
+        trigger_timecode = to_timecode(trigger_time)
 
-        marker_start_time_plus_1_seconds = marker_start_time_seconds + 1
-        marker_start_time_plus_1_tc = seconds_to_timecode(
-            marker_start_time_plus_1_seconds)
+        # source in and out in edl file have to be different so trigger2 is + 1 second to trigger_time
+        trigger2_time = trigger_time + 1
+        trigger2_timecode = to_timecode(trigger2_time)
 
-        timeline_marker = f"{index:03d} 001 V C {marker_start_time_tc} {marker_start_time_plus_1_tc} {marker_start_time_tc} {marker_start_time_plus_1_tc}\n |C:ResolveColor{marker_color} |M:Marker {index} |D:1"
+        timeline_marker = f"{index:03d} 001 V C {trigger_timecode} {trigger2_timecode} {trigger_timecode} {trigger2_timecode}\n |C:ResolveColor{marker_color} |M:Marker {index} |D:1"
         timeline_markers.append(timeline_marker)
 
     return timeline_markers
 
 
-def save_to_file(timeline_markers, output_file):
+def save_file(timeline_markers, output_file):
     with open(output_file, "w") as f:
         for marker in timeline_markers:
             f.write(marker + "\n" + "\n")
 
 
-# Example usage
 if __name__ == "__main__":
     # Set the input and output file paths to be in the same directory as main.py
     script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -77,13 +76,13 @@ if __name__ == "__main__":
     parsed_data = parse_json(input_file_path)
 
     # Generate timeline markers
-    timeline_markers = generate_timeline_markers(parsed_data)
+    markers = gen_markers(parsed_data)
 
     # Save the generated timeline markers to a text file
-    save_to_file(timeline_markers, output_file_path)
+    save_file(markers, output_file_path)
 
     # Print the generated timeline markers for verification
-    for marker in timeline_markers:
+    for marker in markers:
         print(marker)
 
     print(f"\nTimeline markers saved to {output_file_path}")
