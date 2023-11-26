ddba1c785d414ec91cbcc04c91d80a6f7a34453b
Muhammed Yilmaz <yilmaz@email.arizona.edu>
2023-11-26 16:10:37 -0700

    Add GUI using tkinter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import os
+import tkinter as tk
+from tkinter import filedialog, scrolledtext
 
 
 def parse_json(input_file):
@@ -97,36 +99,119 @@ def gen_markers(parsed_data):
     return timeline_markers
 
 
-def save_file(timeline_markers, output_file):
+def save_file(timeline_markers, output_file, log_callback=None):
     """
-    Save generated timeline markers to a .EDL file. To import in Davinci Resolve right click timeline > timelines > import > timeline markers from edl...
+    Save generated timeline markers to a .EDL file. To import in Davinci Resolve right-click timeline > timelines > import > timeline markers from edl...
 
     Parameters:
     - timeline_markers (list): List of strings representing timeline markers.
     - output_file (str): Path to the output file.
+    - log_callback (function): Callback function to update the log.
     """
     with open(output_file, "w") as f:
         for marker in timeline_markers:
             f.write(marker + "\n" + "\n")
+            if log_callback:
+                log_callback(marker + "\n")
+
+
+def process_files(input_file_path, output_file_path, log_callback=None):
+    parsed_data = parse_json(input_file_path)
+    markers = gen_markers(parsed_data)
+    save_file(markers, output_file_path, log_callback)
+
+
+class App:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Timeline Marker Generator")
+        master.minsize(width=360, height=300)
+        master.geometry("360x300")
+
+        # Configure rows and columns for resizing
+        master.columnconfigure(0, weight=1)
+        master.columnconfigure(1, weight=1)
+        master.columnconfigure(2, weight=1)
+        master.rowconfigure(0, weight=0)
+        master.rowconfigure(1, weight=0)
+        master.rowconfigure(2, weight=0)
+        master.rowconfigure(3, weight=0)
+        master.rowconfigure(4, weight=1)
+
+        # First Row
+        self.input_label = tk.Label(master, text="Input File:")
+        self.input_label.grid(row=0, column=0, sticky="e")
+
+        self.input_entry = tk.Entry(master)
+        self.input_entry.grid(row=0, column=1, sticky="ew")
+
+        self.browse_button = tk.Button(master,
+                                       text="Browse",
+                                       command=self.browse_input)
+        self.browse_button.grid(row=0, column=2, sticky="w")
+
+        # Second Row
+        self.output_label = tk.Label(master, text="Output File:")
+        self.output_label.grid(row=1, column=0, sticky="e")
+
+        self.output_entry = tk.Entry(master)
+        self.output_entry.grid(row=1, column=1, sticky="ew")
+
+        self.browse_button = tk.Button(master,
+                                       text="Browse",
+                                       command=self.browse_output)
+        self.browse_button.grid(row=1, column=2, sticky="w")
+
+        self.process_button = tk.Button(master,
+                                        text="Process Files",
+                                        command=self.process_files)
+        self.process_button.grid(row=2, column=1)
+
+        self.log_label = tk.Label(master, text="Log:")
+        self.log_label.grid(row=3, column=1)
+
+        self.log_text = scrolledtext.ScrolledText(master, height=10)
+        self.log_text.grid(row=4, column=0, columnspan=3, sticky="nsew")
+
+    def browse_input(self):
+        file_path = filedialog.askopenfilename(filetypes=[("JSON files",
+                                                           "*.json")])
+        self.input_entry.delete(0, tk.END)
+        self.input_entry.insert(0, file_path)
+
+    def browse_output(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".edl",
+                                                 filetypes=[("EDL files",
+                                                             "*.edl")])
+        self.output_entry.delete(0, tk.END)
+        self.output_entry.insert(0, file_path)
+
+    def process_files(self):
+        input_file_path = self.input_entry.get()
+        output_file_path = self.output_entry.get()
+
+        if input_file_path.lower() == "seattle weather":
+            self.log(
+                "You dont need a weather report for this... you can just assume the weather in seattle is probably cloudy with slight chance of rain at all times. as a general rule of thumb if location == seattle then weather = bad. fun fact there is a guy named murad in seattle who is a mal.\n"
+            )
+
+        if not input_file_path or not output_file_path:
+            self.log("Please select both input and output files.")
+            return
+
+        try:
+            process_files(input_file_path, output_file_path, self.log)
+            self.log("Files processed successfully.")
+        except Exception as e:
+            self.log(f"Error: {str(e)}")
+
+    def log(self, message):
+        self.log_text.insert(tk.END, f"{message}\n")
+        self.log_text.see(tk.END)
 
 
 if __name__ == "__main__":
-    # Set the input and output file paths to be in the same directory as main.py
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    input_file_path = os.path.join(script_directory, "test_clips.json")
-    output_file_path = os.path.join(script_directory, "output_markers.edl")
-
-    # Parse the JSON file
-    parsed_data = parse_json(input_file_path)
-
-    # Generate timeline markers
-    markers = gen_markers(parsed_data)
-
-    # Save the generated timeline markers to a text file
-    save_file(markers, output_file_path)
-
-    # Print the generated timeline markers for verification
-    for marker in markers:
-        print(marker)
-
-    print(f"\nTimeline markers saved to {output_file_path}")
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
